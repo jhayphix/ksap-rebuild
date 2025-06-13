8478dc2647611bdffb9d5a162dff10453a51c523
jhayphix <jhayphix05@gmail.com>
2025-06-13 08:39:25 +0100

    Workon navigation

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -3,16 +3,12 @@ import { useNavigationContext } from "@/contexts/NavigationContextProvider";
 import { RouteObject } from "react-router-dom";
 
 export const usePublicRoutes = (): RouteObject[] => {
-  const nav = useNavigationContext();
+  const { home } = useNavigationContext();
 
   return [
     {
-      path: "/",
+      path: home.path,
       element: <>Home Page</>,
-    },
-    {
-      path: nav.externalScholarships.path,
-      element: <>External Scholarships</>,
     },
   ];
 };
